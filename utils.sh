a212db7292366c63f8cdced6b4d66984069b6cbb
Mark-Asuncion <mark.asuncion.000@gmail.com>
2023-11-13 17:34:11 +0800

    download on /opt/

--- utils.sh
+++ utils.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-if [[ ! -d "$HOME"/Apps ]]; then
-    mkdir "$HOME"/Apps
-fi
 
 echo 'installing nodejs v18.17.1'
 curl -O https://nodejs.org/dist/v18.17.1/node-v18.17.1-linux-x64.tar.xz && \
     tar -xf node-*.tar.xz && \
-    mv node-*/ "$HOME"/Apps/ && \
-    echo "export PATH=\"${HOME}/Apps/node-v18.17.1-linux-x64/bin:\$PATH\"" >> "$HOME"/.usr_conf
+    sudo mv node-*/ /opt/ && \
+    echo "export PATH=\"/opt/node-v18.17.1-linux-x64/bin:\$PATH\"" >> "$HOME"/.usr_conf
 
 echo 'installing neovim'
 wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz && \
     tar -xf nvim-*.tar.gz && \
-    mv nvim-*/ "$HOME"/Apps/ && \
-    echo "export PATH=\"${HOME}/Apps/nvim-linux64/bin:\$PATH\"" >> "$HOME"/.usr_conf
+    sudo mv nvim-*/ /opt/ && \
+    echo "export PATH=\"/opt/nvim-linux64/bin:\$PATH\"" >> "$HOME"/.usr_conf
 
 echo 'setting up nvim config'
 mkdir "$HOME"/.config/nvim/
@@ -23,8 +20,8 @@ echo 'installing lazygit'
 mkdir lazygit_0.40.2_Linux_x86_64
 wget https://github.com/jesseduffield/lazygit/releases/download/v0.40.2/lazygit_0.40.2_Linux_x86_64.tar.gz && \
     tar -xf lazygit*.tar.gz --directory=lazygit_0.40.2_Linux_x86_64 && \
-    mv lazygit*/ "$HOME"/Apps/ && \
-    echo "export PATH=\"${HOME}/Apps/lazygit_0.40.2_Linux_x86_64:\$PATH\"" >> "$HOME"/.usr_conf
+    sudo mv lazygit*/ /opt/ && \
+    echo "export PATH=\"/opt/lazygit_0.40.2_Linux_x86_64:\$PATH\"" >> "$HOME"/.usr_conf
 
 if [[ -z $1 ]]; then
     exit 1
