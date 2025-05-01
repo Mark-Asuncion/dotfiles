d3145c52d2e2081d1204a07decb6076914c70d38
mark-asuncion <mark.asuncion.000@gmail.com>
2025-05-01 20:17:58 +0800

    update

--- kde/setup.sh
+++ kde/setup.sh
@@ -7,21 +7,23 @@ workspace=$(pwd)
 mkdir tmp
 cd tmp
 
-echo 'Installing lightly'
-sudo apt install cmake build-essential \
-    libkf5config-dev libkdecorations2-dev \
-    libqt5x11extras5-dev qtdeclarative5-dev \
-    extra-cmake-modules libkf5guiaddons-dev \
-    libkf5configwidgets-dev libkf5windowsystem-dev \
-    libkf5coreaddons-dev libkf5iconthemes-dev \
-    gettext qt3d5-dev
+function install_lightly {
+    echo 'Installing lightly'
+    sudo apt install cmake build-essential \
+        libkf5config-dev libkdecorations2-dev \
+        libqt5x11extras5-dev qtdeclarative5-dev \
+        extra-cmake-modules libkf5guiaddons-dev \
+        libkf5configwidgets-dev libkf5windowsystem-dev \
+        libkf5coreaddons-dev libkf5iconthemes-dev \
+        gettext qt3d5-dev
 
-git clone --single-branch --depth=1 https://github.com/Luwx/Lightly.git
-cd Lightly && mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_TESTING=OFF ..
-make
-sudo make install
-cd "$workspace"/tmp
+    git clone --single-branch --depth=1 https://github.com/Luwx/Lightly.git
+    cd Lightly && mkdir build && cd build
+    cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_TESTING=OFF ..
+    make
+    sudo make install
+    cd "$workspace"/tmp
+}
 
 function create_dir ()
 {
@@ -29,6 +31,11 @@ function create_dir ()
         mkdir -p $1
     fi
 }
+
+read -p 'Install lightly?[y/n(default)] ' choice
+if [[ $choice == 'y' ]] || [[ $choice == 'Y' ]]; then
+    install_lightly
+fi
 
 echo '-- Installing theme --'
 echo '0 - Nordic (Default)'
